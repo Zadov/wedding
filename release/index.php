6a42e958546a7b0760b7dad99e1f0fca50458e45
Aleksandr <zadov112@ya.ru>
2017-03-23 22:55:33 +0300

    mail, blots

--- release/index.php
+++ release/index.php
@@ -22,62 +22,68 @@
 			<nav class="main-navigation">
 				<ul class="main-navigation__list">
 					<li><a href="#nav-timing">Тайминг</a></li>
-					<li><a href="#nav-">Наши цвета</a></li>
+					<li><a href="#nav-colors">Наши цвета</a></li>
 					<li><a href="#nav-i-will-come">Я приду</a></li>
-					<li><a href="#nav-">Место</a></li>
+					<li><a href="#nav-place">Место</a></li>
 				</ul>
 			</nav>
-			<img class="invite" src="img/invite2.jpg" alt="">
+			<img class="invite" src="img/DSC_8028.png" alt="">
 			<p id="nav-timing" class="time">17 июня 2017<br>Начало в 16:00</p>
 		</div>
 	</header>
 	
 	<main>
 		<div class="container">
-				<form id="nav-i-will-come" action="save_guests.php" method="post" class="guest-form">
-				<div class="guest-form__row">
-					<div class="guest-form__input-block">
-						<input class="guest-form__input" type="text" name="first-name" id="first-name">
-						<label class="guest-form__input-title" for="first-name">Имя:</label>
+			<div class="colors" id="nav-colors">
+				<img class="colors__img" src="img/blots.png" alt="Наши цвета" width="740" height="500">
+			</div>
+
+			<form id="nav-i-will-come" action="save_guests.php" method="post" class="guest-form">
+				<div class="guest-form__paper">
+					<div class="guest-form__row">
+						<div class="guest-form__input-block">
+							<input class="guest-form__input" type="text" name="first-name" id="first-name">
+							<label class="guest-form__input-title" for="first-name">Имя:</label>
+						</div>
+<!-- 					</div>
+
+					<div class="guest-form__row"> -->
+						<div class="guest-form__input-block">
+							<input class="guest-form__input" type="text" name="second-name" id="second-name"">
+							<label class="guest-form__input-title" for="second-name">Фамилия:</label>
+						</div>
 					</div>
-				</div>
 
-				<div class="guest-form__row">
-					<div class="guest-form__input-block">
-						<input class="guest-form__input" type="text" name="second-name" id="second-name"">
-						<label class="guest-form__input-title" for="second-name">Фамилия:</label>
+					<div class="guest-form__row">
+						<input id="will-be-alone" class="guests-form__radio" type="radio" name="will-be" value="alone">
+						<label class="guests-form__radio-title" for="will-be-alone">Я приду</label>
+<!-- 					</div> -->
+
+<!-- 					<div class="guest-form__row"> -->
+						<input id="will-be-pare" class="guests-form__radio" type="radio" name="will-be" value="pare">
+						<label class="guests-form__radio-title" for="will-be-pare">Приду с парой</label>
 					</div>
-				</div>
 
-				<div class="guest-form__row">
-					<input id="will-be-alone" class="guests-form__radio" type="radio" name="will-be" value="alone">
-					<label class="guests-form__radio-title" for="will-be-alone">Я приду</label>
-				</div>
+					<div class="guest-form__row">
+						<input id="will-be-no" class="guests-form__radio" type="radio" name="will-be" value="no">
+						<label class="guests-form__radio-title" for="will-be-no">Не смогу прийти</label>
+					</div>
 
-				<div class="guest-form__row">
-					<input id="will-be-pare" class="guests-form__radio" type="radio" name="will-be" value="pare">
-					<label class="guests-form__radio-title" for="will-be-pare">Приду с парой</label>
-				</div>
+<!-- 					<div class="guest-form__row">
+						<input type="checkbox" id="not-alone" class="not-alone">
+						<label for="not-alone">Я приду не один</label>
+						<details>
+							<summary>reindent</summary>
+							<p><b>Alt+Shift+f</b></p>
+						</details>
+						<details>
+							<summary>забрать гист</summary>
+							<p><b>ctrl+k,ctrl+o</b></p>
+						</details>
+					</div> -->
 
-				<div class="guest-form__row">
-					<input id="will-be-no" class="guests-form__radio" type="radio" name="will-be" value="no">
-					<label class="guests-form__radio-title" for="will-be-no">Не смогу прийти</label>
+					<button class="guest-form__btn">Отправить</button>
 				</div>
-
-				<div class="guest-form__row">
-					<input type="checkbox" id="not-alone" class="not-alone">
-					<label for="not-alone">Я приду не один</label>
-					<details>
-						<summary>reindent</summary>
-						<p><b>Alt+Shift+f</b></p>
-					</details>
-					<details>
-						<summary>забрать гист</summary>
-						<p><b>ctrl+k,ctrl+o</b></p>
-					</details>
-				</div>
-
-				<button>Отправить</button>
 			</form>
 		</div>
 
